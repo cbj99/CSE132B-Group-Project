6067adbcf7bd6caee1f7e9a6a49a79ba3d1b4d1a
cbj99 <bac001@ucsd.edu>
2022-05-02 01:03:13 -0700

    update sql and degree_requirement.jsp

--- createRelationSql/createThesisCommitte.sql
+++ createRelationSql/createThesisCommitte.sql
@@ -3,9 +3,9 @@ CREATE TABLE thesisCommitte(
     faculty_name char(20), 
     PRIMARY KEY(student_id, faculty_name), 
     CONSTRAINT FK_student_id 
-        FOREIGN KEY(student_id) REFERENCES student(student_id)
-        ON DELETE SET NULL, 
+        FOREIGN KEY(student_id) REFERENCES graduateStudent(student_id)
+        ON DELETE CASCADE, 
     CONSTRAINT FK_faculty_name
         FOREIGN KEY(faculty_name) REFERENCES faculty(faculty_name)
-        ON DELETE SET NULL
+        ON DELETE CASCADE
 ); 
