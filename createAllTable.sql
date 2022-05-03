e52900eee7ecac46b9d0ed917b8cea73c7a5393d
Danlandy <74330177+Danlandy@users.noreply.github.com>
2022-05-02 18:13:41 -0700

    sql for all

---
+++ createAllTable.sql
@@ -0,0 +1,383 @@
+create table student(
+    student_id int, 
+	SSN int,  
+    identity_ char(50), 
+    enrollment_status char(20), 
+    first_name char(50), 
+    last_name char(50), 
+    middle_name char(50), 
+    PRIMARY KEY (student_id)
+); 
+
+create table undergraduateStudent(
+    student_id int, 
+	SSN int,  
+    identity_ char(50), 
+    enrollment_status char(20), 
+    first_name char(50), 
+    last_name char(50), 
+    middle_name char(50),  
+    
+    major char(50), 
+    minor char(50), 
+    college char(50), 
+    PRIMARY KEY (student_id),
+    CONSTRAINT FK_student_id 
+        FOREIGN KEY(student_id) REFERENCES student(student_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+
+create table BSMSStudent(
+    student_id int, 
+	SSN int,  
+    identity_ char(50), 
+    enrollment_status char(20),
+    first_name char(50), 
+    last_name char(50), 
+    middle_name char(50), 
+    department char(50), 
+
+    PRIMARY KEY (student_id),
+    CONSTRAINT FK_student_id 
+        FOREIGN KEY(student_id) REFERENCES student(student_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+create table MSStudent(
+    student_id int, 
+	SSN int,  
+    identity_ char(50), 
+    enrollment_status char(20),
+    first_name char(50), 
+    last_name char(50), 
+    middle_name char(50), 
+    department char(50), 
+    PRIMARY KEY (student_id),
+    CONSTRAINT FK_student_id 
+        FOREIGN KEY(student_id) REFERENCES student(student_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+
+
+create table PHDPreCandidacyStudent(
+    student_id int, 
+	SSN int,  
+    identity_ char(50), 
+    enrollment_status char(20),
+    first_name char(50), 
+    last_name char(50), 
+    middle_name char(50), 
+    department char(50), 
+    
+    PRIMARY KEY (student_id),
+    CONSTRAINT FK_student_id 
+        FOREIGN KEY(student_id) REFERENCES student(student_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE probation(
+    student_id int, 
+    year int, 
+    quarter char(10), 
+    university char(20), 
+    reason char(255) not null , 
+
+    PRIMARY KEY(student_id, year, quarter, university),
+    CONSTRAINT FK_student_id 
+        FOREIGN KEY(student_id) REFERENCES student(student_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+); 
+
+CREATE TABLE faculty(
+    faculty_name char(50), 
+    title char(50), 
+    department char(50), 
+
+    PRIMARY KEY(faculty_name)
+); 
+
+
+create table PHDCandidateStudent(
+    student_id int, 
+	SSN int,  
+    identity_ char(50), 
+    enrollment_status char(20),
+    first_name char(50), 
+    last_name char(50), 
+    middle_name char(50), 
+    department char(50), 
+    faculty_name char(50), 
+    PRIMARY KEY (student_id),
+    CONSTRAINT FK_student_id 
+        FOREIGN KEY(student_id) REFERENCES student(student_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE, 
+        
+
+    CONSTRAINT FK_faculty_name
+        FOREIGN KEY(faculty_name) REFERENCES faculty(faculty_name)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+);
+
+create table graduateStudent(
+    student_id int,
+	SSN int,  
+    identity_ char(50), 
+    enrollment_status char(20),
+    first_name char(50), 
+    last_name char(50), 
+    middle_name char(50), 
+    department char(50), 
+    PRIMARY KEY (student_id),
+    CONSTRAINT FK_student_id 
+        FOREIGN KEY(student_id) REFERENCES student(student_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE location(
+    room_number char(20), 
+    building_number char(20), 
+    capacity int,
+
+    PRIMARY KEY(room_number, building_number)
+); 
+
+CREATE TABLE degree_(
+    degree_id char(50), 
+    institution char(50), 
+    degree_name char(50), 
+    department char(20), 
+
+    PRIMARY KEY(degree_id, institution)
+); 
+
+
+CREATE TABLE undergraduateDegree(
+    degree_id char(50), 
+    institution char(50), 
+    degree_name char(50),
+    department char(20),
+    type char(20), 
+    upper_division_units int, 
+    lower_division_units int, 
+
+    PRIMARY KEY(degree_id, institution),
+    CONSTRAINT FK_degree_id 
+        FOREIGN KEY(degree_id, institution) REFERENCES degree_(degree_id, institution) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE graduateDegree(
+    degree_id char(50), 
+    institution char(50), 
+    degree_name char(50),
+    department char(20), 
+
+    PRIMARY KEY(degree_id, institution), 
+    CONSTRAINT FK_degree_id 
+        FOREIGN KEY(degree_id, institution) REFERENCES degree_(degree_id, institution) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE 
+);
+
+CREATE TABLE courses(
+    course_number char(20), 
+    grading_option char(20), 
+    unit int, 
+    department char(20), 
+    lab_required int, 
+
+    PRIMARY KEY(course_number) 
+); 
+
+CREATE TABLE coursePrequisite(
+    course_number char(20), 
+    prerequisite char(20), 
+
+    PRIMARY KEY(course_number, prerequisite),
+    CONSTRAINT FK_course 
+        FOREIGN KEY(course_number) REFERENCES courses(course_number)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT FK_prerequisite
+        FOREIGN KEY(prerequisite) REFERENCES courses(course_number)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE classes(
+    course_number char(20), 
+    year_ int, 
+    quarter char(10), 
+    section_id int, 
+
+    PRIMARY KEY(course_number, year_, quarter, section_id), 
+    CONSTRAINT FK_course_number 
+        FOREIGN KEY(course_number) REFERENCES courses(course_number)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+); 
+
+CREATE TABLE meetings(
+    course_number char(20), 
+    year_ int, 
+    quarter char(10), 
+    section_id int, 
+    type_ char(20), 
+    date_ date NOT NULL DEFAULT CURRENT_DATE, 
+
+    begin_time time NOT NULL DEFAULT CURRENT_TIME,
+    end_time time NOT NULL DEFAULT CURRENT_TIME, 
+    mandatory char(10), 
+    room_number char(20), 
+    building_number char(20),
+
+    PRIMARY KEY(course_number, year_, quarter, section_id, type_, date_),
+    CONSTRAINT FK_classes 
+        FOREIGN KEY(course_number, year_, quarter, section_id) REFERENCES classes(course_number, year_, quarter, section_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE, 
+    CONSTRAINT FK_location
+        FOREIGN KEY(room_number, building_number) REFERENCES location(room_number, building_number)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+); 
+
+/**
+    Relations
+*/
+CREATE TABLE course_offering_ny(
+    faculty_name char(50), 
+    course_number char(20), 
+    year_ int, 
+    quarter char(10), 
+    section_id int,
+
+    PRIMARY KEY(faculty_name, course_number, year_, quarter, section_id),
+    
+    CONSTRAINT FK_faculty_name
+        FOREIGN KEY(faculty_name) REFERENCES faculty(faculty_name)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+
+   CONSTRAINT FK_course_number 
+        FOREIGN KEY(course_number) REFERENCES courses(course_number)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+); 
+
+CREATE TABLE degreeRecord(
+    student_id int,
+    degree_id char(50), 
+    institution char(50),
+    status char(20), 
+
+    PRIMARY KEY(student_id, degree_id, institution), 
+    CONSTRAINT FK_degree
+        FOREIGN KEY(degree_id, institution) REFERENCES degree_(degree_id, institution)
+        ON DELETE CASCADE,
+    CONSTRAINT FK_student_id 
+        FOREIGN KEY(student_id) REFERENCES student(student_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE gradudateDegreeRequiarment(
+    degree_id char(50), 
+    institution char(50),
+    course_number char(20), 
+    is_required char(10), 
+    concentration char(20) NOT NULL, 
+
+    PRIMARY KEY(degree_id, institution, course_number), 
+    CONSTRAINT FK_degree
+        FOREIGN KEY(degree_id, institution) REFERENCES graduateDegree(degree_id, institution)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE, 
+    CONSTRAINT FK_course 
+        FOREIGN KEY(course_number) REFERENCES courses(course_number)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+); 
+
+CREATE TABLE underGradudateDegreeRequiarment(
+    degree_id char(50), 
+    institution char(50),
+    course_number char(20), 
+    is_core char(10), 
+    is_technical_elective char(10), 
+    is_major_elective char(10), 
+    is_college_required char(10), 
+    minimum_grade int, 
+
+    PRIMARY KEY(degree_id, institution, course_number), 
+    CONSTRAINT FK_degree
+        FOREIGN KEY(degree_id, institution) REFERENCES undergraduateDegree(degree_id, institution)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT FK_course 
+        FOREIGN KEY(course_number) REFERENCES courses(course_number)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+); 
+
+CREATE TABLE enrollment(
+    student_id int, 
+    course_number char(20), 
+    year_ int, 
+    quarter char(10), 
+    section_id int, 
+    faculty_name char(20), 
+
+    status char(20), 
+    grade char(20), 
+
+    PRIMARY KEY(student_id, course_number, year_, quarter, section_id, faculty_name), 
+    CONSTRAINT FK_classes 
+        FOREIGN KEY(course_number, year_, quarter, section_id) REFERENCES classes(course_number, year_, quarter, section_id)
+        ON DELETE CASCADE,
+    CONSTRAINT FK_student 
+        FOREIGN KEY(student_id) REFERENCES student(student_id)
+        ON DELETE CASCADE,
+    CONSTRAINT FK_faculty 
+        FOREIGN KEY(faculty_name) REFERENCES faculty(faculty_name)
+        ON DELETE CASCADE
+); 
+
+CREATE TABLE past_enrollment(
+    student_id int, 
+    course_number char(20), 
+    year_ int, 
+    quarter char(10), 
+    
+    grade char(20), 
+
+    PRIMARY KEY(student_id, course_number, year_, quarter), 
+    CONSTRAINT FK_classes 
+        FOREIGN KEY(course_number) REFERENCES courses(course_number)
+        ON DELETE CASCADE,
+    CONSTRAINT FK_student 
+        FOREIGN KEY(student_id) REFERENCES student(student_id)
+        ON DELETE CASCADE
+); 
+
+CREATE TABLE thesisCommittee(
+    student_id int, 
+    faculty_name char(20), 
+    PRIMARY KEY(student_id, faculty_name), 
+    CONSTRAINT FK_student_id 
+        FOREIGN KEY(student_id) REFERENCES graduateStudent(student_id)
+        ON DELETE CASCADE, 
+    CONSTRAINT FK_faculty_name
+        FOREIGN KEY(faculty_name) REFERENCES faculty(faculty_name)
+        ON DELETE CASCADE
+); 
